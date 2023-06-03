a9f1764b185e8f6306b022374b4d88ddcb195512
Larry Villegas <larry_villegas@hotmail.com>
2023-06-03 19:05:13 -0400

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import coinsReducer from './coins/coinsSlice';
 import coinDetailReducer from './coins/detailSlice';
 
-
-const store = configureStore({ 
+const store = configureStore({
   reducer: {
     coins: coinsReducer,
     coin: coinDetailReducer,
-  } 
+  },
 });
 
 export default store;
