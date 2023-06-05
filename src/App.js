59032dd79bc3f0c3590ec99eca434a1cc76febe9
Larry Villegas <larry_villegas@hotmail.com>
2023-06-05 13:21:12 -0400

    Conditional render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { Route, Routes, NavLink } from 'react-router-dom';
+import {
+  Route, Routes, NavLink, useLocation,
+} from 'react-router-dom';
 import './assets/App.css';
 import ListCoins from './components/ListCoins';
 import Details from './components/Details';
@@ -8,22 +10,26 @@ const NotFound = () => <h2>Error 404 - Page not found</h2>;
 const About = () => <h3>About me</h3>;
 
 function App() {
+  const pageLocation = useLocation();
+
   return (
     <div className="page">
       <header>
         <NavLink to="/" className="link">
-          <div className="home">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="48"
-                d="M328 112L184 256l144 144"
-              />
-            </svg>
-          </div>
+          {pageLocation.pathname !== '/' && (
+            <div className="home">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="48"
+                  d="M328 112L184 256l144 144"
+                />
+              </svg>
+            </div>
+          )}
         </NavLink>
         <img
           src="https://cdn.coinranking.com/assets/64374966bb4cd0691a9429341ae9b413.svg"
